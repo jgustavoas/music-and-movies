3ba77ed26e4890a9852865e3a979db78f793c913
jgustavoas <jgustavoas@gmail.com>
2020-06-09 17:18:47 -0300

    Models name in lowercase

--- api/models/movie.js
+++ api/models/movie.js
@@ -1,10 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Movie = sequelize.define('Movie', {
-    movie: DataTypes.STRING,
-    genreId: DataTypes.INTEGER
-  }, {});
-  Movie.associate = function(models) {
+  const Movie = sequelize.define(
+    'movies',
+    {
+      movie: DataTypes.STRING,
+      genreId: DataTypes.INTEGER,
+    },
+    { tableName: 'Movies' }
+  );
+  Movie.associate = function (models) {
     // associations can be defined here
   };
   return Movie;
