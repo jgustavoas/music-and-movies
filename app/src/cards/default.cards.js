9be550b00367360ce76d96f393be7c6e4f8d12fa
jgustavoas <jgustavoas@gmail.com>
2020-06-30 10:47:45 -0300

    Search implemented\

--- app/src/cards/default.cards.js
+++ app/src/cards/default.cards.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import history from '../services/history';
 
 import Button from '../components/elementos/Botao';
 
 export default function DefaultCard({ path, titulo }) {
+  const route = path.split('/')[0];
   const operation = titulo.includes('New') ? 'Create' : 'Search';
+
+  const go = (e) => {
+    const val =
+      e.target.offsetParent.children[1].children.form.children[0].children[
+        operation
+      ].value;
+
+    history.push(`${route}?val=${val}`);
+  };
+
   return (
     <div className='cardContent'>
-      <form>
+      <div id='form'>
         <fieldset>
           <label htmlFor='new'>Name</label>
-          <input type='text' name='' id='new' />
+          <input
+            onKeyDown={(e) => e.key === 'Enter' && go(e)}
+            type='text'
+            name=''
+            id={operation}
+          />
         </fieldset>
-        <Button>{operation}</Button>
-      </form>
+        <Button funcao={go} estilo='cta'>
+          {operation}
+        </Button>
+      </div>
     </div>
   );
 }
