6cc4e93ad5c9e9b28c6216dda6ce7601dfd8f40b
jgustavoas <jgustavoas@gmail.com>
2020-07-01 18:26:25 -0300

    Making saga file for "component" module

---
+++ app/src/store/modules/componentes/sagas.js
@@ -0,0 +1,21 @@
+import { takeLatest, call, all } from 'redux-saga/effects';
+import { store } from '../../index';
+import { acao } from './actions';
+
+import api from '../../../services/api';
+
+export function* toList({ payload }) {
+  let { item: source, data } = payload;
+  const { path, by } = data;
+
+  try {
+    const response = yield call(api.get, `${path}?by=${by}`);
+    const data = { options: response.data };
+
+    store.dispatch(acao('FORM:READY', source, path, data));
+  } catch (err) {
+    store.dispatch(acao('FORM:FAIL'));
+  }
+}
+
+export default all([takeLatest('FORM', toList)]);
