9a38d02cb8e35b945ca53d13ec35c9a4ea133a9f
jgustavoas <jgustavoas@gmail.com>
2020-07-01 18:36:06 -0300

    Dynamic form fields

--- app/src/cards/default.cards.js
+++ app/src/cards/default.cards.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { store } from '../store';
 import request from '../store/modules/data/actions';
 
+import { doNotSubmit, getOptions } from '../functions/form.func';
+
 import history from '../services/history';
 import Button from '../components/elementos/Botao';
 import { columns } from '../objetos/columns.obj';
@@ -11,37 +13,41 @@ export default function DefaultCard({ path, titulo }) {
   const route = path.split('/')[0];
   const wichOne = titulo.includes('New') ? 'Create' : 'Search';
 
+  const { card, form } = store.getState().componentes;
   const [firstColumn] = columns[route];
   const [, col] = firstColumn;
 
   const go = (e) => {
-    const val =
-      e.target.offsetParent.children[1].children.form.children[0].children[
-        wichOne
-      ].value;
+    console.log('e.target.form :>> ', e.target.form);
+    const val = e.target.form[0].value;
 
     if (wichOne === 'Search') history.push(`${route}?val=${val}`);
     else store.dispatch(request('CREATE', 'card', route, { [col]: val }));
   };
 
+  card && !form.ready && getOptions();
+
   return (
     <div className='cardContent'>
-      <div id='form'>
-        <fieldset>
-          <label htmlFor={wichOne}>Name</label>
-          <input
-            onKeyDown={(e) => {
-              e.key === 'Enter' && go(e);
-            }}
-            type='text'
-            name=''
-            id={wichOne}
-          />
-        </fieldset>
+      <form onSubmit={doNotSubmit()}>
+        {columns[route].map((field, index) => {
+          const [labelField, nameField] = field;
+          return (
+            <section key={index}>
+              <label htmlFor={wichOne}>{labelField}</label>
+              <input
+                onKeyDown={(e) => e.key === 'Enter' && go(e)}
+                type='text'
+                name={nameField}
+                id={nameField}
+              />
+            </section>
+          );
+        })}
         <Button funcao={go} estilo='cta'>
           {wichOne}
         </Button>
-      </div>
+      </form>
     </div>
   );
 }
