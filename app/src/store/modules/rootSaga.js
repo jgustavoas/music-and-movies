3d2df12a1e39c5b55aa8a28725c6f146c7005682
jgustavoas <jgustavoas@gmail.com>
2020-07-01 18:31:19 -0300

    Importing saga from "components" module

--- app/src/store/modules/rootSaga.js
+++ app/src/store/modules/rootSaga.js
@@ -2,7 +2,8 @@ import { all } from 'redux-saga/effects';
 
 import auth from './auth/sagas';
 import data from './data/sagas';
+import components from './componentes/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, data]);
+  return yield all([auth, data, components]);
 }
