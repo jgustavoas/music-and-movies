51a4b3c64dc60a1b78f7d39e62aba96d674cd04c
jgustavoas <jgustavoas@gmail.com>
2020-07-02 17:41:17 -0300

    Refactoring the "Input" component

--- app/src/components/elementos/Input.js
+++ app/src/components/elementos/Input.js
@@ -1,63 +1,37 @@
-import React, { useState } from 'react';
-import { CampoDeInput } from '../../styles/Input.style';
+import React from 'react';
+import { keyEvent } from '../../functions/form.func';
 
-export default function Componente(props) {
-  const {
-    tipo,
-    valido,
-    id,
-    textoDoLabel,
-    placeholder,
-    valor,
-    classeCSS,
-  } = props;
+const input = {
+  text: function (name) {
+    return <input onKeyDown={keyEvent} type='text' name={name} id={name} />;
+  },
+  autocomplete: function (name) {
+    return <input onKeyDown={keyEvent} type='text' name={name} id={name} />;
+  },
+  select: function (name) {
+    return (
+      <select name={name} id={name}>
+        <option value=''>Select one...</option>
+        {this.options.map((option, index) => {
+          return (
+            <option key={index} value={index}>
+              {option.genre}
+            </option>
+          );
+        })}
+      </select>
+    );
+  },
+};
 
-  const [stateValor, setValorInput] = useState(valor);
-  const [atributos, setAtributos] = useState({ valido, placeholder });
-
-  function atualizarValorDoInput(valor) {
-    setValorInput(valor);
-
-    if (classeCSS !== 'Sidebar.inputs') {
-      const regexpEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-      if (valor.length < 4 || (tipo === 'email' && !valor.match(regexpEmail))) {
-        setAtributos({
-          valido: false,
-          placeholder,
-        });
-      } else {
-        setAtributos({
-          valido: true,
-          placeholder,
-        });
-      }
-    }
-  }
-
-  function alterarAtributo() {
-    !valido &&
-      setAtributos({
-        valido: !valido,
-        placeholder,
-      });
-  }
+export default function Componente({ settings }) {
+  const [labelField, nameField, type, options] = settings;
+  input.options = options;
 
   return (
-    <CampoDeInput valido={atributos.valido}>
-      <label className={classeCSS} htmlFor={id}>
-        {textoDoLabel}
-      </label>
-      <input
-        onFocus={() => alterarAtributo()}
-        onChange={(e) => atualizarValorDoInput(e.target.value)}
-        type={tipo}
-        name={id}
-        id={id}
-        value={stateValor}
-        placeholder={
-          valido ? atributos.placeholder : 'Preencha este campo corretamente!'
-        }
-      />
-    </CampoDeInput>
+    <section>
+      <label htmlFor={nameField}>{labelField}</label>
+      {input[type](nameField)}
+    </section>
   );
 }
