f253df9546d9c9e673a260130c4e7b69a000148e
jgustavoas <jgustavoas@gmail.com>
2020-07-03 11:40:44 -0300

    Moving the form validation function to "form.func.js" file

--- app/src/functions/gerais.func.js
+++ app/src/functions/gerais.func.js
@@ -3,41 +3,6 @@ import request from '../store/modules/data/actions';
 
 import { ItensDoMenu } from '../objetos/menuNav.obj';
 
-// VALIDAÇÃO DE FORMULÁRIO:
-// Esta função recebe como parâmetro uma array []
-// Os inputs a serem validados ficam dentro de uma segunda array no seguinte formato: [tipoDoInput, valorDoInput]
-// Ou seja, os inputs são inseridos na função como neste exemplo: [['email', $valor], [senha, $valor]]
-export function validarForm(inputs) {
-  let isValid = true; // O input começa válido por padrão.
-
-  return inputs.map((dadosDoInput) => {
-    // Usando map() para a array de cada input e desetruturando a array
-    const [tipoDoInput, valorDoInput] = dadosDoInput;
-
-    // Expressão regular para validar o formato do e-mail
-    // É uma camada a mais de validação além da que já existe nos navegadores para o input do tipo "email".
-    const regexpEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-
-    // Se o input for do tipo 'email' e seu valor não der match com a expressão regular acima, então invalidEmail = true.
-    const invalidEmail =
-      tipoDoInput === 'email' && !valorDoInput.match(regexpEmail);
-
-    // Se o input for undefined, ou vazio, ou for um e-mail inválido, então exibir mensagem de erro.
-    if (
-      !valorDoInput ||
-      valorDoInput === '' ||
-      valorDoInput.length < 4 ||
-      invalidEmail
-    ) {
-      isValid = false;
-    } else {
-      isValid = true;
-    }
-
-    return isValid;
-  });
-}
-
 // Quando há novo foco no input invalidado, limpar o CSS e o placeholder do input
 export function removerAtributo(id) {
   document.getElementById(id).removeAttribute('class', 'invalid');
