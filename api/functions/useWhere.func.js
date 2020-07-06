8c45dc484b5e5483ab372eede08fcc19016e6adc
jgustavoas <jgustavoas@gmail.com>
2020-07-06 15:38:43 -0300

    Simplifying the "useWhere()" function

--- api/functions/useWhere.func.js
+++ api/functions/useWhere.func.js
@@ -1,27 +1,23 @@
 var Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
-function requireWhere(inc, col, val) {
-  inc.find((model) => {
-    const foundModel = model.attributes.include.some(
-      (column) => column === col
-    );
-
-    if (foundModel) {
-      model.where = { [col]: { [Op.iLike]: `%${val}%` } }; // see footer note #1
-      model.required = true; // see footer note #1
-    }
-  });
-}
-
 module.exports = (include, rest) =>
   Object.entries(rest).forEach((and) => {
     const [col, val] = and;
-    requireWhere(include, col, val);
+
+    include.find((model) => {
+      const foundModel = model.attributes.include.some(
+        (column) => column === col
+      );
+
+      if (foundModel) {
+        model.where = { [col]: { [Op.iLike]: `%${val}%` } }; // #1
+        model.required = true; // #1
+      }
+    });
   });
 
 /*
-  FOOTER NOTES:
-    1) Check about "options.include[].where" and "options.include[].required":
+    #1 Check about "options.include[].where" and "options.include[].required":
     https://sequelize.org/v5/class/lib/model.js~Model.html#static-method-findAll
 */
