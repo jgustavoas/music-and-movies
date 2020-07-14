7361bad707bc0e46346166fb45018008a43ee50f
jgustavoas <jgustavoas@gmail.com>
2020-07-14 09:17:57 -0300

    Using class to construct inputs

--- app/src/objetos/inputs.obj.js
+++ app/src/objetos/inputs.obj.js
@@ -1,69 +1,19 @@
-import React from 'react';
-
-import { Input } from '../styles/Input.style';
-import { keyEvent, getOptions } from '../functions/form.func';
+import { Input, Datalist, Select } from './inputsConstructor.obj';
 
 export const input = {
   text(name, isValid) {
-    return (
-      <Input
-        isValid={isValid}
-        onKeyDown={keyEvent}
-        type='text'
-        name={name}
-        id={name}
-      />
-    );
+    return new Input(name).construct(isValid);
+  },
+  email(name, isValid) {
+    return new Input(name, 'email').construct(isValid);
   },
   password(name, isValid) {
-    return (
-      <Input
-        isValid={isValid}
-        onKeyDown={keyEvent}
-        type='password'
-        name={name}
-        id={name}
-      />
-    );
+    return new Input(name, 'password').construct(isValid);
   },
   datalist(name, isValid, list) {
-    return (
-      <>
-        <Input
-          onChange={(e) => {
-            getOptions(list.model);
-          }}
-          list={name}
-          name={name}
-          isValid={isValid}
-          onKeyDown={keyEvent}
-        />
-        <datalist id={name}>
-          {list.data.map((option, index) => {
-            return (
-              <option
-                id={option.id}
-                key={index}
-                value={option[name.slice(0, -2)]}
-              />
-            );
-          })}
-        </datalist>
-      </>
-    );
+    return new Datalist(name, list).construct(isValid);
   },
   select(name, isValid, options) {
-    return (
-      <select name={name} id={name}>
-        <option value='none'>Select one...</option>
-        {options.map((option, index) => {
-          return (
-            <option key={index} value={option.id}>
-              {option.genre}
-            </option>
-          );
-        })}
-      </select>
-    );
+    return new Select(name, options).construct(isValid);
   },
 };
---
+++ app/src/objetos/inputsConstructor.obj.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import { Input as Component, Select as Sel } from '../styles/Input.style';
+
+import { keyEvent, getOptions } from '../functions/form.func';
+
+export class Input {
+  constructor(name, type = 'text') {
+    this.name = name;
+    this.type = type;
+  }
+
+  construct(isValid, model) {
+    return (
+      <Component
+        onChange={() => {
+          return model ? getOptions(model) : null;
+        }}
+        list={this.name}
+        name={this.name}
+        type={this.type}
+        isValid={isValid}
+        onKeyDown={keyEvent}
+      />
+    );
+  }
+}
+
+export class Datalist extends Input {
+  constructor(name, list) {
+    super(name);
+    this.list = list;
+  }
+
+  construct(isValid) {
+    const textInput = super.construct(isValid, this.list.model);
+
+    return (
+      <>
+        {textInput}
+        <datalist id={this.name}>
+          {this.list.data.map((option, index) => {
+            return (
+              <option
+                id={option.id}
+                key={index}
+                value={option[this.name.slice(0, -2)]}
+              />
+            );
+          })}
+        </datalist>
+      </>
+    );
+  }
+}
+
+export class Select {
+  constructor(name, options) {
+    this.name = name;
+    this.options = options;
+  }
+
+  construct(isValid) {
+    return (
+      <Sel isValid={isValid} name={this.name} id={this.name}>
+        <option value='none'>Select one...</option>
+        {this.options.map((option, index) => {
+          return (
+            <option key={index} value={option.id}>
+              {option.genre}
+            </option>
+          );
+        })}
+      </Sel>
+    );
+  }
+}
