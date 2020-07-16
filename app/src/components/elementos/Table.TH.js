e0eebe32d580e6b12f20fa3b38869c490cdc91c4
jgustavoas <jgustavoas@gmail.com>
2020-07-16 09:44:23 -0300

    Revert "Refactoring table elements"
    This reverts commit 57302a3661fb54deaa67a0e644fd18bf0e475566.

--- app/src/components/elementos/Table.TH.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function TH(coluna, indice) {
-  const [tituloDaColuna] = coluna;
-  const { source } = this;
-  const identificacao = `0_${indice}_${tituloDaColuna}_${source}`;
-
-  return <th key={identificacao}>{tituloDaColuna}</th>;
-}
--- app/src/components/elementos/Thead.js
+++ app/src/components/elementos/Thead.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import TH from './Table.TH';
 
 export default function Thead({ source, path, variaveis }) {
   const [linhas] = variaveis;
+
   const colunas = linhas[1] ? linhas[1].colunas : []; // Só renderiza as colunas quando houver dados.
-  const obj = { source, style: 'thead_tr' };
 
   return (
     <thead id={`thead_${source}`}>
       <tr key={0} className={`thead_tr`}>
-        {colunas.map(TH, obj)}
+        {colunas.map((coluna, indice) => {
+          const [tituloDaColuna] = coluna;
+          const identificacao = `0_${indice}_${tituloDaColuna}_${source}`;
+          return <th key={identificacao}>{tituloDaColuna}</th>;
+        })}
       </tr>
     </thead>
   );
