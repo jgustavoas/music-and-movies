c0e78382f45a8e7c6442341638c2457f9be655ab
jgustavoas <jgustavoas@gmail.com>
2020-07-16 09:30:07 -0300

    Cleaning up comments

--- app/src/components/Table.js
+++ app/src/components/Table.js
@@ -1,32 +1,28 @@
-// REACT -------------------------------------------------------------------------------------------
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-// COMPONENTES -------------------------------------------------------------------------------------
-import Thead from './elementos/Thead';
-import Tbody from './elementos/Tbody';
+// components
+import Thead from './elementos/Table.Thead';
+import Tbody from './elementos/Table.Tbody';
 import Paginacao from './Paginacao';
 
-// FUNÇÕES -----------------------------------------------------------------------------------------
+// functions
 import { loadDados } from '../functions/componente.func';
 
-// NOVO COMPONENTE ---------------------------------------------------------------------------------
 export default function Table({ fonte: source, path }) {
   const CRUD = useSelector((state) => state.data[source]);
   const { loaded, data, error, query } = CRUD;
   const { page, limit } = query;
 
-  // Definindo states:
+  // setting states
   const [linhas, setLinhas] = useState([{ id: 0, nome: '' }]);
   const [pagina, setPagina] = useState(page);
   const [paginacao, setPaginacao] = useState([]);
   const [status, setStatus] = useState('LOADING...');
 
   useEffect(() => {
-    // Voltar à pagina 1 quando o componente remontar:
     setPagina(1);
 
-    // Atualizar o state das linhas da tabela e também o state da paginação com o dados obtidos:
     data && loadDados(data, source, [setStatus, setLinhas, setPaginacao]);
   }, [data, loaded, source]);
 
